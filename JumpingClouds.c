3823fc779475bcea0da478741be33c57efea422d
Divya Bharathy Venkatraman <60822129+divyabharathy@users.noreply.github.com>
2020-06-13 11:27:14 +0530

    Create JumpingClouds.c

---
+++ JumpingClouds.c
@@ -0,0 +1,123 @@
+/*Emma is playing a new mobile game that starts with consecutively numbered clouds. Some of the clouds are thunderheads and others are cumulus. She can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus 1 or 2. She must avoid the thunderheads. Determine the minimum number of jumps it will take Emma to jump from her starting postion to the last cloud. It is always possible to win the game.
+
+For each game, Emma will get an array of clouds numbered 0 if they are safe or 1 if they must be avoided. For example, [0, 1, 0, 0, 0, 1, 0] indexed from 0..6 . The number on each cloud is its index in the list so she must avoid the clouds at indexes  1 and 5. She could follow the following two paths: 0->2->4->6 or 0->2->3->4->6. The first path takes 3 jumps while the second takes 4.*/
+
+
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+// Complete the jumpingOnClouds function below.
+int jumpingOnClouds(int c_count, int* c) 
+{
+    int i;
+    int s=0,k=0;
+    for(i=0;i<c_count-1;i++)
+    {
+        if(*(c+i+1)==0)
+        {
+            s++;
+            if(*(c+i+2)==0&&*(c+i)==0)
+            {
+                i++;
+            }
+        }
+    }
+    return s;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* n_endptr;
+    char* n_str = readline();
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char** c_temp = split_string(readline());
+
+    int* c = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        char* c_item_endptr;
+        char* c_item_str = *(c_temp + i);
+        int c_item = strtol(c_item_str, &c_item_endptr, 10);
+
+        if (c_item_endptr == c_item_str || *c_item_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        *(c + i) = c_item;
+    }
+
+    int c_count = n;
+
+    int result = jumpingOnClouds(c_count, c);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
